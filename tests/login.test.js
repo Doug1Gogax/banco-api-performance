a503ccef0c3795be0553e15fd0e1e890011fa7d6
Initial config <douglas.w.estevao@gmail.com>
2025-08-13 16:29:13 -0300

    Mensagem do commit para o submódulo

---
+++ tests/login.test.js
@@ -0,0 +1,38 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+
+export const options = {
+  iterations: 50,
+  thresholds:{
+    http_req_duration:['p(90)<1', 'max<1'],
+    http_req_failed: ['rate<0.01']
+
+  }
+};
+
+export default function () {
+  //teste!
+  const url = "http://localhost:3000/login";
+
+  const payload = JSON.stringify({
+    username: "julio.lima",
+    senha: "123456",
+  });
+
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const res = http.post(url, payload, params);
+
+  check(res,{
+    'Validar que o Status é 200': (r) => r.status === 200,
+    'Validar que o Token e string': (r) => typeof(r.json().token) == 'string'
+  })
+
+  sleep(1);
+
+}
